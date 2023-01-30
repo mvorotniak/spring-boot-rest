e04ab5c7b4a93586ee828887712e7f83ec4517f1
Mariana Vorotniak <mvorodeveloper@gmail.com>
2023-01-30 07:14:42 -0800

    Moved baseUrl to properties file

--- src/main/java/com/example/springbootrest/controller/CustomerController.java
+++ src/main/java/com/example/springbootrest/controller/CustomerController.java
@@ -2,14 +2,11 @@ package com.example.springbootrest.controller;
 
 import java.util.stream.Collectors;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootrest.api.CustomerDTO;
 import com.example.springbootrest.api.CustomersDTO;
@@ -19,13 +16,14 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller
+@RequestMapping("${app.base.url}")
 public class CustomerController {
 
     private final CustomerService customerService;
 
     private final CustomerMapper customerMapper;
 
-    @GetMapping("/rest/customers/all")
+    @GetMapping("/all")
     public ResponseEntity<CustomersDTO> findAllCustomers() {
         final CustomersDTO customersDTO = CustomersDTO.builder()
             .customerDTOS(this.customerService.getAllCustomers().stream()
@@ -36,7 +34,7 @@ public class CustomerController {
         return ResponseEntity.ok(customersDTO);
     }
 
-    @GetMapping("/rest/customers/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<CustomerDTO> findCustomerById(@PathVariable final Long id) {
         final CustomerDTO customerDTO = this.customerMapper.toDTOEntity(this.customerService.getCustomerById(id));
 
