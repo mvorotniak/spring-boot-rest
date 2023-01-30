4349ba73006fd36d17e3185e0a6e75b137e8510c
Mariana Vorotniak <mvorodeveloper@gmail.com>
2023-01-30 07:35:31 -0800

    changed controller to restcontroller

--- src/main/java/com/example/springbootrest/controller/CustomerController.java
+++ src/main/java/com/example/springbootrest/controller/CustomerController.java
@@ -2,11 +2,12 @@ package com.example.springbootrest.controller;
 
 import java.util.stream.Collectors;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootrest.api.CustomerDTO;
 import com.example.springbootrest.api.CustomersDTO;
@@ -15,7 +16,7 @@ import com.example.springbootrest.service.CustomerService;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-@Controller
+@RestController
 @RequestMapping("${app.base.url}")
 public class CustomerController {
 
@@ -24,21 +25,19 @@ public class CustomerController {
     private final CustomerMapper customerMapper;
 
     @GetMapping("/all")
-    public ResponseEntity<CustomersDTO> findAllCustomers() {
-        final CustomersDTO customersDTO = CustomersDTO.builder()
+    @ResponseStatus(HttpStatus.OK)
+    public CustomersDTO findAllCustomers() {
+        return CustomersDTO.builder()
             .customerDTOS(this.customerService.getAllCustomers().stream()
                 .map(this.customerMapper::toDTOEntity)
                 .collect(Collectors.toList()))
             .build();
-
-        return ResponseEntity.ok(customersDTO);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CustomerDTO> findCustomerById(@PathVariable final Long id) {
-        final CustomerDTO customerDTO = this.customerMapper.toDTOEntity(this.customerService.getCustomerById(id));
-
-        return ResponseEntity.ok(customerDTO);
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerDTO findCustomerById(@PathVariable final Long id) {
+        return this.customerMapper.toDTOEntity(this.customerService.getCustomerById(id));
     }
 
 }
